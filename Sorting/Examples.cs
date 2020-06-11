c6c94a6f84a3c7d7cc832d9979772a0facc041d2
Stratis Dermanoutsos <38103639+Stratis-Dermanoutsos@users.noreply.github.com>
2020-06-11 10:45:57 +0300

    Usage Examples

---
+++ Sorting/Examples.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace Sorting_Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float[] floats = new float[] { 6.9f, 2.5f, 2.1f, 23.0f, 12, 2, 3, 6 };
+            int[] integers = new int[] { 9, 6, 5, 3, 10, 1, 8, 7, 2, 4 };
+            char[] characters = new char[] { 'a', 'e', 'r', 'd', 'v', 'z', 'b', 'c' };
+
+            Console.WriteLine("Bubble Sort (float): " + new BubbleSort<float>(floats).ToString());
+            Console.WriteLine("Bubble Sort (int): "+ new BubbleSort<int>(integers).ToString());
+            Console.WriteLine("Bubble Sort (char): " + new BubbleSort<char>(characters).ToString());
+            Console.WriteLine("\n");
+            Console.WriteLine("Selection Sort (float): " + new SelectionSort<float>(floats).ToString());
+            Console.WriteLine("Selection Sort (int): " + new SelectionSort<int>(integers).ToString());
+            Console.WriteLine("Selection Sort (char): " + new SelectionSort<char>(characters).ToString());
+            Console.WriteLine("\n");
+            Console.WriteLine("Insertion Sort (float): " + new InsertionSort<float>(floats).ToString());
+            Console.WriteLine("Insertion Sort (int): " + new InsertionSort<int>(integers).ToString());
+            Console.WriteLine("Insertion Sort (char): " + new InsertionSort<char>(characters).ToString());
+        }
+    }
+}
