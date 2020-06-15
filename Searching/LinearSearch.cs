ea004c39ed0db12e9226a5196d9d6c771fdd0c02
Stratis Dermanoutsos <38103639+Stratis-Dermanoutsos@users.noreply.github.com>
2020-06-15 08:14:33 +0300

    Linear Search method

---
+++ Searching/LinearSearch.cs
@@ -0,0 +1,8 @@
+public int LinearSearch(T[] list, T item) // O(n)
+{
+  for (int i = 0; i < list.Length; i++)
+    if (Comparer<T>.Default.Compare(list[i], item) == 0)
+      return i; // If found, return the index
+
+  return -1; // Else, return -1
+}
