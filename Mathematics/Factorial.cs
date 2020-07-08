0e124f26bdacf142dddbe5e4add3637b35a70fdf
Stratis Dermanoutsos <38103639+Stratis-Dermanoutsos@users.noreply.github.com>
2020-07-08 13:55:24 +0300

    n!

---
+++ Mathematics/Factorial.cs
@@ -0,0 +1,8 @@
+System.Numerics.BigInteger Factorial(int n) // We start counting from 0
+{
+  if (n < 0) return -1; // n must be non-negative
+
+  if (n < 2) return 1; // both 0! and 1! equal 1
+
+  return n * Factorial(n - 1); // n! = n*(n-1)*(n-2)*...*1
+}
