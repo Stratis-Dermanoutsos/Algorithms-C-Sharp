25ef158f432e9742b4108d0a8d2d7ddb01aa725b
Stratis Dermanoutsos <38103639+Stratis-Dermanoutsos@users.noreply.github.com>
2020-07-14 10:54:47 +0300

    n-th Fibonacci number

--- Mathematics/Fibonacci.cs
+++ Mathematics/Fibonacci.cs
@@ -2,7 +2,7 @@ public System.Numerics.BigInteger Fibonacci(int n) // We start counting from 0
 {
   if (n < 0) return -1; // n must be non-negative
 
-  if (n < 2) return n; // f(0) = 0 and f(1) = 1
+  if (n < 2) return n; // for n < 2, Fn = n
 
-  return Fibonacci(n - 1) + Fibonacci(n - 2); // f(n) = f(n-1) + f(n-2)
+  return Fibonacci(n - 1) + Fibonacci(n - 2); // Fn = Fn-1 + Fn-2
 }
