f37b432a8664f81be5b5a2e331cb01599eb8a244
Stratis Dermanoutsos <38103639+Stratis-Dermanoutsos@users.noreply.github.com>
2020-08-11 18:13:41 +0300

    Square number recursive

--- Mathematics/RecursiveSquare.cs
+++ Mathematics/RecursiveSquare.cs
@@ -1,4 +1,8 @@
 long RecursiveSquare(int n)
 {
-  return (n == 1 || n == 0) ? n : RecursiveSquare(n-1) + n + n-1;
+  if (n >= 0) // For non-negative n
+    return (n == 1 || n == 0) ? n : RecursiveSquare(n - 1) + n + n - 1;
+
+  // For negative n
+  return (n == -1) ? 1 : RecursiveSquare(-n + 1) + n + n - 1;
 }
